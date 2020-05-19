50c46c9b9ff8c2c4cb7de1977e4f75953bc4cbb6
Shadan Abdulkarim <shadanabdulkarim94@gmail.com>
2020-05-19 21:24:18 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-  const inpuTask=document.getElementsByTagName("input")[0];
+  const inpuTask=document.getElemetById("new-task-description");
     const inputButton=document.getElementsByTagName("input")[1];
     const Ultasks=document.getElemetById('tasks');
     let liArr=[]
