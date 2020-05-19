774d1fda10251d77a9fe89661e621c36b303f7cb
Shadan Abdulkarim <shadanabdulkarim94@gmail.com>
2020-05-19 21:07:51 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
   // your code here
   const inpuTask=document.getElemetById('new-task-description');
     const inputButton=document.getElemetById('new-task-description');
+    const Ultasks=document.getElemetById('tasks');
 inputButton.addEventListener('click',adding())
 function adding(){
   
