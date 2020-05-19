095669c8f0065cadb0a15bad2cb9c4238f598b2a
Shadan Abdulkarim <shadanabdulkarim94@gmail.com>
2020-05-19 21:37:53 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  
+  
 });
+const form=document.getElementById('create-task-form')
+const inpuTask=document.getElementById("new-task-description");
+    const inputButton=document.getElementsByTagName("input")[1];
+    const Ultasks=document.getElementById('tasks');
+    let liArr=[];
+    
+    form.addEventListener('submit',(event)=>{
+      event.preventDefault();
+        const items=document.createElement('li')
+  items.innerText=inpuTask.value
+  Ultasks.appendChild(items)
+    });
+
+
+
+
