c6781a570334778600f6e07a649204440fd07c22
Shadan Abdulkarim <shadanabdulkarim94@gmail.com>
2020-05-19 21:09:39 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ document.addEventListener("DOMContentLoaded", () => {
   const inpuTask=document.getElemetById('new-task-description');
     const inputButton=document.getElemetById('new-task-description');
     const Ultasks=document.getElemetById('tasks');
+    const liArr=[]
 inputButton.addEventListener('click',adding())
 function adding(){
-  
+  const items=document.createElement('li')
 }
   
 });
