8dee63f3ef0b8db323546b37d54c53c9e575a92d
Shadan Abdulkarim <shadanabdulkarim94@gmail.com>
2020-05-19 21:05:52 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-  document.getElemetById('new-task-description');
-  
+  const inpuTask=document.getElemetById('new-task-description');
+    const inputButton=document.getElemetById('new-task-description');
+
   
 });
